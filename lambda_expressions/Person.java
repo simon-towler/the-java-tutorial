9919bdfd84c3eda562720ef415d8e17183f76d23
simon-towler <simon.towler@icloud.com>
2019-10-04 19:04:15 +0100

    Added new class Person

---
+++ lambda_expressions/Person.java
@@ -0,0 +1,22 @@
+import java.lang.String;
+import java.time.LocalDate;
+
+public class Person {
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+
+    public int getAge() {
+        // ...
+    }
+
+    public void printPerson() {
+        // ...
+    }
+}
