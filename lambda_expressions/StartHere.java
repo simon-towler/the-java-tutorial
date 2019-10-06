56d6a6297f4e09c796ed395e4389b4c48fae16cb
simon-towler <simon.towler@icloud.com>
2019-10-06 15:23:07 +0100

    Approach 3: Specify search criteria code in a nested class.

--- lambda_expressions/StartHere.java
+++ lambda_expressions/StartHere.java
@@ -7,7 +7,8 @@ public class StartHere {
     public static void main(String[] args){
 
         List<Person> roster = Person.createRoster();
-        printPersonsWithinAgeRange(roster, 20, 60);
+        //printPersonsWithinAgeRange(roster, 20, 60);
+        printPersons(roster, new CheckPersonElligibleForSelectiveService());
 
     System.out.println("Done!");
     }
@@ -25,6 +26,23 @@ public class StartHere {
     public static void printPersonsWithinAgeRange(List<Person> roster, int low, int high) {
         for (Person p : roster) {
             if (low <= p.getAge() && p.getAge() < high) {
+                p.printPerson();
+            }
+        }
+    }
+
+    static class CheckPersonElligibleForSelectiveService {
+        public boolean test (Person p) {
+            return p.gender == Person.Sex.MALE &&
+                    p.getAge() >= 18 &&
+                    p.getAge() <= 25;
+        }
+    }
+
+    //approach 3: Specify search criteria code in a local class
+    public static void printPersons(List<Person> roster, CheckPersonElligibleForSelectiveService tester) {
+        for (Person p : roster) {
+            if (tester.test(p)) {
                 p.printPerson();
             }
         }
