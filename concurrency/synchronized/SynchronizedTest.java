90c50d24b7f6fe245688356af2445195a38f0bcb
simon-towler <simon.towler@icloud.com>
2020-09-20 07:52:51 +0100

    Added comments

--- concurrency/synchronized/SynchronizedTest.java
+++ concurrency/synchronized/SynchronizedTest.java
@@ -3,7 +3,14 @@ public class SynchronizedTest {
   public static void main(String... args) {
    SynchronizedRGB colour = new SynchronizedRGB(0, 0, 0, "Pitch Black");
 
+   /*
+   If another thread invokes color.set after statement 1 but before statement 2,
+   the value of myColourInt won't match the value of myColourName. To avoid that
+   the two statements would have to be 'bound together' in some way.
+   */
+   // statement 1
    int myColourInt = colour.getRGB();
+   // statement 2
    String myColourName = colour.getName();
   }
 
